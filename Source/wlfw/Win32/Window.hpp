b79c40b077d7dd73647993beeaa50a7569b6b20e
eUltrabyte <goszymon1337@gmail.com>
2022-08-21 11:02:11 +0200

    wlfw.hpp changed

---
+++ Source/wlfw/Win32/Window.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef WLFW_WINDOW_WIN32_HEADER
+#define WLFW_WINDOW_WIN32_HEADER
+
+#include "../wlfw.hpp"
+
+namespace wl {
+    class WLFW_API Window {
+    public:
+
+    private:
+
+    };
+};
+
+#endif
--- Source/wlfw/wlfw.hpp
+++ Source/wlfw/wlfw.hpp
@@ -4,10 +4,38 @@
 
 #include "wlfwpch.hpp"
 
-#ifdef WLFW_UNIX_WINDOW_PLATFORM_WIN32
-
+#ifdef WLFW_SHARED_LIBRARY
+    #ifdef WLFW_SHARED_BUILD
+        #ifdef _MSC_VER
+            #define WLFW_API __declspec(dllexport)
+        #else
+            #define WLFW_API __attribute__((visibility("default")))
+        #endif
+    #else
+        #ifdef _MSC_VER
+            #define WLFW_API __declspec(dllimport)
+        #else
+            #define WLFW_API __attribute__((visibility("hidden")))
+        #endif
+    #endif
 #else
+    #define WLFW_API
+#endif
 
+#ifdef WLFW_WINDOW_PLATFORM_WIN32
+    #include <Windows.h>
+    #include <windowsx.h>
+#else
+    #ifdef WLFW_UNIX_WINDOW_PLATFORM_X11
+        #include <X11/X11.h>
+        #include <X11/Xlib.h>
+    #elif WLFW_UNIX_WINDOW_PLATFORM_XCB
+        #include <xcb/xcb.h>
+    #elif WLFW_UNIX_WINDOW_PLATFORM_WAYLAND
+        #include <wayland-client.h>
+        #include <wayland-client-core.h>
+        #include <wayland-client-protocol.h>
+    #endif
 #endif
 
 #ifdef WLFW_DEBUG
@@ -17,5 +45,7 @@
 #endif
 
 #define WLFW_CHECK(value) if(!value) { WLFW_TRACE(); }
+
+#include "Win32/Window.hpp"
 
 #endif
